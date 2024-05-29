e0ff83ba64995d03c7e89b21d2d98faeeb452453
andreastif <45339645+andreastif@users.noreply.github.com>
2024-05-29 09:39:05 +0200

    first commit

---
+++ src/components/StepsPage.tsx
@@ -0,0 +1,3 @@
+export const StepsPage = () => {
+  return <>stepspage</>;
+};
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      robot: ["Roboto Mono", "Roboto", "sans-serif"],
+    },
+  },
+};
+
+// eslint-disable-next-line no-undef
+export const plugins = [require("@tailwindcss/typography"), require("daisyui")];
+
+export const daisyui = {
+  themes: ["light", "dark"],
+};
